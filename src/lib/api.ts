7baf24e20ceb440beeb24ffac1678dfc5a02b761
sh3nz <45-canary.queue@icloud.com>
2025-02-12 12:41:42 -0500

    Update api.ts
    adding Oauth token and username as per reddit API compliance

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -9,7 +9,8 @@
 //         method: 'GET',
 //         headers: {
 //           'Content-Type': 'application/json',
-//           // Add any other required headers
+//           'User-Agent': 'web:com.commentextractor.app:v1.0.0 (by /u/Jpeg30286)',
+//           'Authorization': `Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjpzS3dsMnlsV0VtMjVmcXhwTU40cWY4MXE2OWFFdWFyMnpLMUdhVGxjdWNZIiwidHlwIjoiSldUIn0.eyJzdWIiOiJsb2lkIiwiZXhwIjoxNzM5NDY4MDkyLjk0NDI1MywiaWF0IjoxNzM5MzgxNjkyLjk0NDI1MywianRpIjoiZE5MUzVWZDBsZ1pOamEyc2Q3cEFiMTFmRFU4OUhnIiwiY2lkIjoicDkxOHpqcVdLRU55SlZTajVXRTJFUSIsImxpZCI6InQyXzFqNHIyNGd2dGMiLCJsY2EiOjE3MzkzODE2OTI5MzAsInNjcCI6ImVKeUtWdEpTaWdVRUFBRF9fd056QVNjIiwiZmxvIjo2fQ.FNndtd2_hnZWkfjqYN9sNTTNvaEU0Re8dmaI9Jdg-9EhrqNEcKaHygxbG4ZwsU-LhydOwi5YfjGl-c6hTzrEsS5HjV5VqLiVcPM5G5CyFPB3uW1J99Ck4ys4YyKVl2ehWxesMw2CoG9leOyokf7MrE4MUfosDr78VBipfUuwxdz5oB7t7OkTV3qA6j7DIZsYbfhT9xXfOdII4x6XbXiMhFcv2Uohx05jYCHcSU76MNrYPfoBfZzkUdm2dV2pwrU03cF7Djy39AvvfOrystPAQ0f-4YUiPXPspuju4GUAOYSyh9b97n2ucE9K9mVwHkuQVGHXn_jkT-6Wr-6jy4sGEw`
 //         },
 //         // Important for CORS
 //         mode: 'cors',
